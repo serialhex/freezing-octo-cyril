84577db10027b0409b46f6cb18fbbfb024dbb15a
Justin Patera <serialhex@gmail.com>
2012-06-09 11:29:06 -0400

    made the repo a rails one

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreezingOctoCyril::Application.config.secret_token = '3c0cf624ba23e72a1295445720645c3c71bb87a76b60c35e729fa0b951da20ec63be8fb91f8d825a410a907ab2ae00f8ea14a06791b52ce41de5c583d5be972c'
